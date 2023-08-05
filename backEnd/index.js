c7664b70ae0db188204d6294c3cf00f4d4f05435
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 18:42:12 +0530

    small changes

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,24 +2,24 @@ const express = require('express');
 const TodoRoute = require('./routes/todo.route');
 const  connect  = require('./db/connectDatabase');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path')
 app.use(bodyParser.json()); // To parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use('/todos', TodoRoute);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
 app.use(express.static('public'))
-app.use(express.json())
+// app.use(express.json())
 
 
-// app.get('/*', (req,res)=>{
-//     res.sendFile(path.join(__dirname, './public/index.html',));
-// })
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html',));
+})
 
 connect().then(()=>{
     app.listen(3000, ()=>{
