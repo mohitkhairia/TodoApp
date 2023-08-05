2b2bf40495969bdf6e9a671ff5f35b2d4fc547b7
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 18:52:05 +0530

    changed port number

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const TodoRoute = require('./routes/todo.route');
 const  connect  = require('./db/connectDatabase');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path')
 app.use(bodyParser.json()); // To parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 app.use('/todos', TodoRoute);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
 app.use(express.static('public'))
-// app.use(express.json())
+app.use(express.json())
 
 
 app.get('/*', (req,res)=>{
@@ -22,8 +22,8 @@ app.get('/*', (req,res)=>{
 })
 
 connect().then(()=>{
-    app.listen(3000, ()=>{
-        console.log("Server started on http://localhost:3000")
+    app.listen(3001, ()=>{
+        console.log("Server started on http://localhost:3001")
     })
     }
     )
