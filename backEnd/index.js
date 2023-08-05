9881fd1afb22a1ed761412e76206458594f3aff5
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 14:48:31 +0530

    small cors issue

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const TodoRoute = require('./routes/todo.route');
 const  connect  = require('./db/connectDatabase');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path')
 app.use(bodyParser.json()); // To parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/todos', TodoRoute);
-app.use(cors())
+// app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
