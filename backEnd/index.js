a5358cf8e3d638d9fbc593247f03d69f36e2a60c
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 18:22:22 +0530

    allow acess from anywhere

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,23 +7,23 @@ const bodyParser = require('body-parser');
 const path = require('path')
 app.use(bodyParser.json()); // To parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/todos', TodoRoute);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-// app.use(express.static('public'))
+app.use(express.static('public'))
 app.use(express.json())
 
 
-// app.get('/*', (req,res)=>{
-//     res.sendFile(path.join(__dirname, './public/index.html',));
-// })
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html',));
+})
 
 connect().then(()=>{
-    app.listen(3000, ()=>{
-        console.log("Server started on http://localhost:3000")
+    app.listen(3001, ()=>{
+        console.log("Server started on http://localhost:3001")
     })
     }
     )
