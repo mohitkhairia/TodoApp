3a1229d0eb7355b7e023316477e75856050079c7
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 14:45:17 +0530

    coars update

--- backEnd/index.js
+++ backEnd/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json()); // To parse JSON data
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors({ origin: 'http://localhost:3000' }));
 app.use('/todos', TodoRoute);
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
