a62a6c8a9b69eafeec8b5ba5528e0a4e920facb7
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 18:34:30 +0530

    removed path security

--- backEnd/index.js
+++ backEnd/index.js
@@ -17,13 +17,13 @@ app.use(express.static('public'))
 app.use(express.json())
 
 
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html',));
-})
+// app.get('/*', (req,res)=>{
+//     res.sendFile(path.join(__dirname, './public/index.html',));
+// })
 
 connect().then(()=>{
-    app.listen(3001, ()=>{
-        console.log("Server started on http://localhost:3001")
+    app.listen(3000, ()=>{
+        console.log("Server started on http://localhost:3000")
     })
     }
     )
