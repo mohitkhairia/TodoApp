4abf7578c3bb3fefd76022865ee45605e8a09e97
mohitkhairia <mohitkhairia@gmail.com>
2023-08-05 17:28:55 +0530

    switching off the send file feature on all the other routes

--- backEnd/index.js
+++ backEnd/index.js
@@ -14,9 +14,9 @@ app.use(express.static('public'))
 app.use(express.json())
 
 
-app.get('/*', (req,res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html',));
-})
+// app.get('/*', (req,res)=>{
+//     res.sendFile(path.join(__dirname, './public/index.html',));
+// })
 
 connect().then(()=>{
     app.listen(3000, ()=>{
